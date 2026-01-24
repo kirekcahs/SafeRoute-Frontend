9ad8e938cb6c6732f7d01d7139969ee8d80bfc2f
iamvinxz <john.khervin13@gmail.com>
2026-01-24 12:54:03 +0800

    updates

--- app/index.jsx
+++ app/index.jsx
@@ -1,158 +1,214 @@
-import Feather from '@expo/vector-icons/Feather';
-import { LinearGradient } from 'expo-linear-gradient';
-import React from 'react';
-import { Image, ImageBackground, ScrollView, StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import Feather from "@expo/vector-icons/Feather";
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import {
+  Image,
+  ImageBackground,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const rainyLogo = require('../assets/images/rainy-logo.png')
-const sunnyLogo = require('../assets/images/sunny-logo.png')
-const sunny = require('../assets/images/cloud-blue-sky.jpg')
-const rainy = require('../assets/images/rainy-weather.jpg')
+const rainyLogo = require("../assets/images/rainy-logo.png");
+const sunnyLogo = require("../assets/images/sunny-logo.png");
+const sunny = require("../assets/images/cloud-blue-sky.jpg");
+const rainy = require("../assets/images/rainy-weather.jpg");
 const Home = () => {
-
   return (
-    <LinearGradient className='w-full h-full'
-      colors={["#DBD9D9","#6E8DE0"]}
-      start={{x:0, y:0}}
-      end={{x: 1, y:1}}
+    <LinearGradient
+      className="w-full h-full"
+      colors={["#DBD9D9", "#6E8DE0"]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
     >
-      <SafeAreaView>
-        
-        <View className='p-7'>
-          <Text className='text-[1.5rem] pb-5' style={{fontFamily: "Poppins-Medium"}}>Today's Weather</Text>
+      <ScrollView
+        vertical
+        contentContainerStyle={{ paddingHorizontal: 5 }}
+        showsHorizontalScrollIndicator={false}
+      >
+        <SafeAreaView>
+          <View className="p-7">
+            <Text
+              className="text-[1.5rem] pb-5"
+              style={{ fontFamily: "Poppins-Medium" }}
+            >
+              Today's Weather
+            </Text>
 
             <ImageBackground source={sunny} style={style.image}>
-                <LinearGradient className='absolute' style={StyleSheet.absoluteFill}
-                  colors={['rgba(255, 255, 255, 0.70)', 'rgba(191, 186, 186, 0.50)']}>
+              <LinearGradient
+                className="absolute"
+                style={StyleSheet.absoluteFill}
+                colors={[
+                  "rgba(255, 255, 255, 0.70)",
+                  "rgba(191, 186, 186, 0.50)",
+                ]}
+              >
+                {/**weather temp */}
+                <View className="flex justify-center items-center pt-5">
+                  <Text style={style.location}>Malabon</Text>
+                  <Text style={style.temperature}>33 °C</Text>
 
-                    {/**weather temp */}
-                    <View className='flex justify-center items-center pt-5'>
+                  <View className="flex-row items-center">
+                    <Image source={sunnyLogo} style={style.weatherImage} />
+                    <Text style={style.weather}>Sunny</Text>
+                  </View>
+                </View>
 
-                      <Text style={style.location}>Malabon</Text>
-                      <Text style={style.temperature}>33 °C</Text>
+                {/*other stats */}
+                <View className="flex-row justify-evenly mx-4 mt-2">
+                  {/**wind speed*/}
+                  <View className="flex-row gap-2 items-center ">
+                    <Feather name="wind" size={18} color="#393636" />
+                    <Text className="text-sm">22km</Text>
+                  </View>
+                  {/**humidity*/}
+                  <View className="flex-row gap-2 items-center ">
+                    <Feather name="droplet" size={18} color="#393636" />
+                    <Text className="text-sm">23%</Text>
+                  </View>
+                </View>
 
-                      <View className='flex-row items-center'>
-                        <Image source={sunnyLogo} style={style.weatherImage}/>
-                        <Text style={style.weather}>Sunny</Text>
-                      </View>
-
-                    </View>
-
-                      {/*other stats */}
-                    <View className='flex-row justify-evenly mx-4 mt-2'>
-
-                      //wind speed
-                      <View className='flex-row gap-2 items-center '>
-                        <Feather name="wind" size={18} color="#393636" />
-                        <Text className='text-sm'>22km</Text>
-                      </View>
-
-                      //humidity
-                       <View className='flex-row gap-2 items-center '>
-                        <Feather name="droplet" size={18} color="#393636" />
-                        <Text className='text-sm'>23%</Text>
-                      </View>
-                       
-                    </View>
-
-                    <ScrollView horizontal contentContainerStyle={{paddingHorizontal: 5}} showsHorizontalScrollIndicator={false}>
-                      <View className='bg-[rgba(255,255,255,0.43)] flex justify-center items-center w-24 rounded-3xl p-2 my-5 mx-1 gap-2'>
-                          <Image source={rainyLogo} style={style.forecastImage}/>
-                          <Text style={style.forecastFont}>Monday</Text>
-                      </View>
-                      <View className='bg-[rgba(255,255,255,0.43)] flex justify-center items-center w-24 rounded-3xl p-3 my-5 mx-1 gap-2'>
-                          <Image source={sunnyLogo} style={style.forecastImage}/>
-                          <Text style={style.forecastFont}>Tuesday</Text>
-                      </View>
-                      <View className='bg-[rgba(255,255,255,0.43)] flex justify-center items-center w-24 rounded-3xl p-3 my-5 mx-1 gap-2'>
-                          <Image source={rainyLogo} style={style.forecastImage}/>
-                          <Text style={style.forecastFont}>Wednesday</Text>
-                      </View>
-                      <View className='bg-[rgba(255,255,255,0.43)] flex justify-center items-center w-24 rounded-3xl p-3 my-5 mx-1 gap-2'>
-                          <Image source={sunnyLogo} style={style.forecastImage}/>
-                          <Text style={style.forecastFont}>Thusday</Text>
-                      </View>
-                      <View className='bg-[rgba(255,255,255,0.43)] flex justify-center items-center w-24 rounded-3xl p-3 my-5 mx-1 gap-2'>
-                          <Image source={rainyLogo} style={style.forecastImage}/>
-                          <Text style={style.forecastFont}>Friday</Text>
-                      </View>
-                      <View className='bg-[rgba(255,255,255,0.43)] flex justify-center items-center w-24 rounded-3xl p-3 my-5 mx-1 gap-2'>
-                          <Image source={rainyLogo} style={style.forecastImage}/>
-                          <Text style={style.forecastFont}>Saturday</Text>
-                      </View>
-                      <View className='bg-[rgba(255,255,255,0.43)] flex justify-center items-center w-24 rounded-3xl p-3 my-5 mx-1 gap-2'>
-                          <Image source={rainyLogo} style={style.forecastImage}/>
-                          <Text style={style.forecastFont}>Sunday</Text>
-                      </View>
-                    </ScrollView>
-                </LinearGradient>
+                <ScrollView
+                  horizontal
+                  contentContainerStyle={{ paddingHorizontal: 5 }}
+                  showsHorizontalScrollIndicator={false}
+                >
+                  <View className="bg-[rgba(255,255,255,0.43)] flex justify-center items-center w-24 rounded-3xl p-2 my-5 mx-1 gap-2">
+                    <Image source={rainyLogo} style={style.forecastImage} />
+                    <Text style={style.forecastFont}>Monday</Text>
+                  </View>
+                  <View className="bg-[rgba(255,255,255,0.43)] flex justify-center items-center w-24 rounded-3xl p-3 my-5 mx-1 gap-2">
+                    <Image source={sunnyLogo} style={style.forecastImage} />
+                    <Text style={style.forecastFont}>Tuesday</Text>
+                  </View>
+                  <View className="bg-[rgba(255,255,255,0.43)] flex justify-center items-center w-24 rounded-3xl p-3 my-5 mx-1 gap-2">
+                    <Image source={rainyLogo} style={style.forecastImage} />
+                    <Text style={style.forecastFont}>Wednesday</Text>
+                  </View>
+                  <View className="bg-[rgba(255,255,255,0.43)] flex justify-center items-center w-24 rounded-3xl p-3 my-5 mx-1 gap-2">
+                    <Image source={sunnyLogo} style={style.forecastImage} />
+                    <Text style={style.forecastFont}>Thusday</Text>
+                  </View>
+                  <View className="bg-[rgba(255,255,255,0.43)] flex justify-center items-center w-24 rounded-3xl p-3 my-5 mx-1 gap-2">
+                    <Image source={rainyLogo} style={style.forecastImage} />
+                    <Text style={style.forecastFont}>Friday</Text>
+                  </View>
+                  <View className="bg-[rgba(255,255,255,0.43)] flex justify-center items-center w-24 rounded-3xl p-3 my-5 mx-1 gap-2">
+                    <Image source={rainyLogo} style={style.forecastImage} />
+                    <Text style={style.forecastFont}>Saturday</Text>
+                  </View>
+                  <View className="bg-[rgba(255,255,255,0.43)] flex justify-center items-center w-24 rounded-3xl p-3 my-5 mx-1 gap-2">
+                    <Image source={rainyLogo} style={style.forecastImage} />
+                    <Text style={style.forecastFont}>Sunday</Text>
+                  </View>
+                </ScrollView>
+              </LinearGradient>
             </ImageBackground>
+          </View>
 
-        </View>
+          <View className="mb-5">
+            <Text
+              className="text-[1.5rem] py-5 pl-7"
+              style={{ fontFamily: "Poppins-Medium" }}
+            >
+              Articles
+            </Text>
 
-        <View>
-            <Text className='text-[1.5rem] py-5 pl-7' style={{fontFamily: "Poppins-Medium"}}>Articles</Text>
-        </View>
+            <View>
+              <ScrollView
+                horizontal
+                contentContainerStyle={{ paddingHorizontal: 30 }}
+                showsHorizontalScrollIndicator={false}
+              >
+                <View className="w-[9rem] bg-[rgba(255,255,255,0.43)] mr-5 h-[10rem] rounded-xl">
+                  <Text></Text>
+                </View>
 
-      </SafeAreaView>
+                <View className="w-[9rem] bg-[rgba(255,255,255,0.43)] mr-5 h-[10rem] rounded-xl">
+                  <Text></Text>
+                </View>
+
+                <View className="w-[9rem] bg-[rgba(255,255,255,0.43)] mr-5 h-[10rem] rounded-xl">
+                  <Text></Text>
+                </View>
+
+                <View className="w-[9rem] bg-[rgba(255,255,255,0.43)] mr-5 h-[10rem] rounded-xl">
+                  <Text></Text>
+                </View>
+              </ScrollView>
+            </View>
+          </View>
+
+          <View>
+            <Text
+              className="text-[1.5rem] py-5 pl-7"
+              style={{ fontFamily: "Poppins-Medium" }}
+            >
+              Things to remember{" "}
+            </Text>
+          </View>
+        </SafeAreaView>
+      </ScrollView>
     </LinearGradient>
-  )
-}
+  );
+};
 
 const style = StyleSheet.create({
   shadow: {
-  shadowColor: "#000",
-  shadowOffset: {
-    width: 0,
-    height: 12,
-  },
-  shadowOpacity: 0.24,
-  shadowRadius: 15.38,
-  elevation: 19,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.24,
+    shadowRadius: 15.38,
+    elevation: 19,
   },
   image: {
     width: 300,
     height: 275,
     borderRadius: 10,
-    overflow: 'hidden',
+    overflow: "hidden",
     alignSelf: "center",
-     shadowColor: "#000",
+    shadowColor: "#000",
     shadowOffset: {
-    width: 0,
-    height: 12,
+      width: 0,
+      height: 12,
     },
     shadowOpacity: 0.24,
-   shadowRadius: 15.38,
+    shadowRadius: 15.38,
     elevation: 19,
   },
   location: {
     fontFamily: "Poppins-Medium",
     fontSize: 18,
-    color: "#393636"
+    color: "#393636",
   },
   temperature: {
     fontFamily: "Poppins-Regular",
     fontSize: 40,
-    color: "#393636"
+    color: "#393636",
   },
   weather: {
     fontFamily: "Poppins-Regular",
     fontSize: 15,
-    color: "#393636"
+    color: "#393636",
   },
   weatherImage: {
     width: 30,
-    height: 20
+    height: 20,
   },
   forecastImage: {
     width: 40,
-    height: 30
+    height: 30,
   },
   forecastFont: {
-    fontFamily: 'Poppins-Regular',
-    fontSize: 12
-  }
+    fontFamily: "Poppins-Regular",
+    fontSize: 12,
+  },
+});
 
-})
-
-export default Home
+export default Home;
--- app/menu.jsx
+++ app/menu.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Menu = () => {
   return (
-    <View>
-      <Text>Menu</Text>
-    </View>
-  )
-}
+    <LinearGradient
+      className="w-full h-full"
+      colors={["#DBD9D9", "#6E8DE0"]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+    >
+      <SafeAreaView>
+        <View className="w-[90%] bg-[#F6F2F2] self-center mt-10 rounded-xl">
+          <View className="p-5">
+            <Text className="self-center text-xl font-medium mb-5">
+              Profile
+            </Text>
+            <View className="flex-row gap-10 w-full">
+              <View>
+                <Text style={style.font}>Name: </Text>
+                <Text style={style.font}>Phone Number: </Text>
+                <Text style={style.font}>Location: </Text>
+              </View>
+              {/**profile info */}
+              <View>
+                <Text style={style.font}>John Doe</Text>
+                <Text style={style.font}>09123456789</Text>
+                <Text style={style.font}>Malabon City</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
+    </LinearGradient>
+  );
+};
 
-export default Menu
+const style = StyleSheet.create({
+  font: {
+    fontFamily: "Poppins-Regular",
+    fontSize: 14,
+    color: "#393636",
+    paddingVertical: 5,
+  },
+});
+
+export default Menu;
